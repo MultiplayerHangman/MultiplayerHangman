c608d012c1e8bea2c82b84e25a34e9d88167af1e
alextwin <31052458+alextwin@users.noreply.github.com>
2018-02-09 17:36:28 -0500

    Implemented underlined phrase

--- hangman.py
+++ hangman.py
@@ -3,7 +3,16 @@ class Hangman:
   def __init__(self, phrase):
     # Set the phrase to guess
     self.phrase = phrase
-
+    self.numChars = len(self.phrase)
+    self.underlinePhrase = ""
+    
+    for x in range(0, self.numChars):
+        if self.phrase[x] == " ":
+            self.underlinePhrase = self.underlinePhrase + " "
+        else:
+            self.underlinePhrase = self.underlinePhrase + "_"
+            
+            
   # Guess the letter
   def guess(self, letter):
     print("Guessing the letter " + letter + " in " + self.phrase)
@@ -11,10 +20,10 @@ class Hangman:
   # Get the phrase back with underlines for what has not yet been guessed
   # Ex. for "hello world", and guesses "e", return ["_e___ _____"]
   def getCurrentlyCorrectPhrase(self):
-    return "_____ _____"
+    return self.underlinePhrase
 
 if __name__ == '__main__':
-  hm = Hangman(raw_input("Enter phrase to guess: "))
+  hm = Hangman(input("Enter phrase to guess: "))
   while (True):
-    hm.guess(raw_input("Enter letter: "))
-    hm.getCurrentlyCorrectPhrase()
+    hm.guess(input("Enter letter: "))
+    print(hm.getCurrentlyCorrectPhrase())
