5b13194dc28e985a82c64822fbfa2a1f7c7ea582
Andy Cho <hello@andycho.ca>
2018-02-09 18:07:04 -0500

    Add SocketIO and create connection event

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 from hangman import Hangman
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello():
   return render_template('index.html')
+
+@socketio.on('connection')
+def handle_client_connection(json):
+  print('Received connection from client with data: ' + str(json))
+
+if __name__ == '__main__':
+    socketio.run(app)
--- static/js/hangman.js
+++ static/js/hangman.js
@@ -49,3 +49,9 @@ function keyPressed() {
   	}
   }
 }
+
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function() {
+  socket.emit('connection', {data: 'I\'m connected!'});
+});
+
