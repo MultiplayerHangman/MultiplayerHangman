0307d5d676c8e6fb6c7258e4fc6ab8accba3b35f
MultiplayerHangman <ericliang@v1022-rn-34-23.campus-dynamic.uwaterloo.ca>
2018-02-23 03:17:44 -0500

    Interaction with Titlescreen Text
    * new screen width and height
    * player is now an object with properties and methods
    * interaction between buttons and text input
    * different style of text input and added aethetics

--- static/js/hangman.js
+++ static/js/hangman.js
@@ -1,23 +1,20 @@
-const screenWidth = 1000;
-const screenHeight = 600;
-let playerName;
-let userNamed;
+const screenWidth = 1080;
+const screenHeight = 700;
 let titleScreen, playScreen;
+let player;
 let socket = io.connect('http://' + document.domain + ':' + location.port);
 
 function setup() {
   const canvas = createCanvas(screenWidth,screenHeight);
-  // Sets text stroke colour to white
-  stroke(255);
-  // Sets text fill colour to white
-  fill(255);
+
   // Put the canvas inside the #sketch-holder div
   canvas.parent('sketch-holder');
 
   titleScreen = true;
   playScreen = false;
-  userNamed = false;
-  playerName = "";
+  
+  // Creates a new instance of playerInfo() to store user data
+  player = new playerInfo();
 }
 
 function draw() {
@@ -25,38 +22,66 @@ function draw() {
   clear();
   // Displays player's name (just for testing)
   if (titleScreen) {
+    stroke(255);
+    fill(255);
   	textSize(80);
   	textAlign(CENTER);
-  	text("HANGMAN",screenWidth/2,screenHeight/4);
-  	textSize(30);
-  	textAlign(LEFT);
-  	text("Name: " + playerName,200,250);
+    // Title of titlescreen
+  	text("HANGMAN", screenWidth/2, screenHeight/3);
+    // Name of player in name bar
+    textSize(30);
+    text(player.playerName, screenWidth/2, screenHeight/3 + 100);
+
+    rectMode(RADIUS);
+    if (!player.userConfirmed) {
+      noFill();
+    } else {
+      fill(255, 40);
+    }
+    // Frame of name bar
+    rect(screenWidth/2, screenHeight/3 + 89, 240, 25);
+
+    if (player.playerName.length == 0) {
+      stroke(210);
+      fill(210);
+      // Filler text in empty name bar
+      text("Your Name", screenWidth/2, screenHeight/3 + 100);
+    }
   }
 }
+
+// This is an object created to store the user's player info
+function playerInfo() {
+  this.playerName = "";
+  this.userConfirmed = false;
+
+  this.resetPlayer = function() {
+    this.playerName = "";
+    this.userConfirmed = false;
+  };
+};
+
 
 function keyPressed() {
-  if (!userNamed) {
-  	if (keyCode==BACKSPACE && playerName.length>0) {
-  		// Cuts off last letter in the player's name
-  		playerName = playerName.substring(0,playerName.length-1);
-  	} else if (keyCode!=ENTER && playerName.length<30) {
-  		// Concatenates the typed letter to the player's name in lowercase
-  		if (keyIsDown(SHIFT)) {
-  			playerName += key;
-  		} else {
-  			playerName += key.toLowerCase();
-  		}
-  	} else if (keyCode==ENTER) {
-  		// Stops the naming process
-  		userNamed = true;
-  	}
+  if (!player.userConfirmed) {
+  	player.playerName = textModify(player.playerName, 30);
   }
 }
 
-function mousePressed() {
 
+// General purpose text input function used exclusively in keyPressed()
+function textModify(text, maxStringLength) {
+  if (keyCode == BACKSPACE && text.length > 0) {
+    text = text.substring(0, text.length - 1);
+  } else if (keyCode != BACKSPACE && text.length < maxStringLength) {
+    if (keyIsDown(SHIFT)) {
+      text += key;
+    } else {
+      text += key.toLowerCase();
+    }
+  }
+  return text;
 }
-
 
 
 $('#reset').click(function() {
