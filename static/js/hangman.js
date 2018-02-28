b3ee7ab96f862181e3d7fe0282a3a7cf2ae76f90
MultiplayerHangman <ericliang@v1020-wn-192-37.campus-dynamic.uwaterloo.ca>
2018-02-28 16:12:08 -0500

    Organized and Cleaned Up
    Divided code into sections with descriptions.

--- static/js/hangman.js
+++ static/js/hangman.js
@@ -1,6 +1,6 @@
 const screenWidth = 1080;
 const screenHeight = 700;
-let player, titleScreen, playScreen;
+let player, titleScreen, loadingScreen, gameScreen;
 let socket = io.connect('http://' + document.domain + ':' + location.port);
 
 
@@ -11,88 +11,134 @@ function setup() {
   canvas.parent('sketch-holder');
 
   titleScreen = true;
-  playScreen = false;
+  loadingScreen = false;
+  gameScreen = false;
 
   // Creates a new instance of playerInfo() to store user data
   player = new playerInfo();
 }
+
+
+// Overall Control Flow /////////////////////////////////////////////////////////////
+
 
 function draw() {
   // Repeatedly updates the screen
   clear();
 
   if (titleScreen) {
-    // Temporary event listeners
-    document.getElementById("become-chooser").addEventListener("click", function(){
-      if (player.playerName.trim().length > 0) {
-        player.userConfirmed = true;
-        player.userType = "chooser";
-        document.getElementById("become-chooser").style.backgroundColor = "rgb(100,100,100)";
-        document.getElementById("become-guesser").disabled = true;
-        document.getElementById("become-chooser").disabled = true;
-      }
-    });
-
-    document.getElementById("become-guesser").addEventListener("click", function(){
-      if (player.playerName.trim().length > 0) {
-        player.userConfirmed = true;
-        player.userType = "guesser";
-        document.getElementById("become-guesser").style.backgroundColor = "rgb(100,100,100)";
-        document.getElementById("become-chooser").disabled = true;
-        document.getElementById("become-guesser").disabled = true;
-      }
-    });
-
-    document.getElementById("reset").addEventListener("click", function(){
-      player.resetPlayer();
-    });
-
-    textAlign(CENTER);
-    stroke(255);
-    fill(255);
-    textSize(18);
-    text("MULTIPLAYER", screenWidth/2, screenHeight/4 - 20);
-    textSize(80);
-    // Title of titlescreen
-    text("HANGMAN", screenWidth/2, screenHeight/3);
-    // Name of player in name bar
-    textSize(30);
-    text(player.playerName, screenWidth/2, screenHeight/3 + 100);
-
-    rectMode(RADIUS);
-    if (!player.userConfirmed) {
-      noFill(); // empty rectangle (not filled)
-    } else {
-      fill(255, 40);
-    }
-    // Frame of name bar
-    rect(screenWidth/2, screenHeight/3 + 89, 160, 25);
-
-    if (player.playerName.trim().length == 0) {
-      stroke(210);
-      fill(210);
-      // Filler text in empty name bar
-      text("Your Nickname", screenWidth/2, screenHeight/3 + 100);
-    }
+    drawTitleScreen();
+  } else if (loadingScreen) {
+    player.userType = "guesser";
+    drawLoadingScreen();
   }
 }
 
-// This is an object created to store the user's player info
+
+// User Player Info /////////////////////////////////////////////////////////////////
+
+
 function playerInfo() {
   this.playerName = "";
   this.userConfirmed = false; // whether the user has confirmed their user type
   this.userType = "";
 
   this.resetPlayer = function() {
-    this.playerName = "";
     this.userConfirmed = false;
     this.userType = "";
-    document.getElementById("become-chooser").style.backgroundColor = "transparent";
-    document.getElementById("become-guesser").style.backgroundColor = "transparent";
-    document.getElementById("become-chooser").disabled = false;
-    document.getElementById("become-guesser").disabled = false;
+  };
+
+  this.becomeChooser = function() {
+    this.playerName = this.playerName.trim();
+    this.userConfirmed = true;
+    this.userType = "chooser";
+    // alert("became chooser"); // for testing
+  };
+
+  this.becomeGuesser = function() {
+    this.playerName = this.playerName.trim();
+    this.userConfirmed = true;
+    this.userType = "guesser";
+    // alert("became guesser"); // for testing
   };
 };
+
+
+// Program Screen Definitions /////////////////////////////////////////////////////////
+
+
+function drawTitleScreen() {
+
+  textAlign(CENTER);
+  stroke(255);
+  fill(255);
+
+  // Title of titlescreen
+  textSize(18);
+  text("MULTIPLAYER", screenWidth/2, screenHeight/4 - 20);
+  textSize(80);
+  text("HANGMAN", screenWidth/2, screenHeight/3);
+
+  // Name of player in name bar
+  textSize(30);
+  text(player.playerName, screenWidth/2, screenHeight/3 + 100);
+
+  
+  if (player.userConfirmed) {
+    push(); // Seperate style for loading text
+
+      textSize(16);
+      textStyle(ITALIC);
+      strokeWeight(0.5);
+
+      // Loading text when player has confirmed
+      if (player.userType == "guesser") {
+        text("Waiting for a chooser...", screenWidth/2, 2*screenHeight/3 + 40);
+      } else if (player.userType == "chooser") {
+        text("Waiting for a guesser...", screenWidth/2, 2*screenHeight/3 + 40);
+      }
+
+    pop(); // Removes temporary style
+
+    fill(255, 40);
+  } else {
+    noFill();
+  }
+
+  // Frame of name bar
+  rectMode(RADIUS);
+  rect(screenWidth/2, screenHeight/3 + 89, 160, 25);
+
+  if (player.playerName.trim().length == 0) {
+    stroke(210);
+    fill(210);
+
+    // Filler text in empty name bar
+    text("Your Nickname", screenWidth/2, screenHeight/3 + 100);
+  }
+}
+
+
+function drawLoadingScreen() {
+  textAlign(CENTER);
+  stroke(255);
+  fill(255);
+  if (player.userType == "guesser") {
+    textSize(20);
+    text("Game in Session", screenWidth/2, 50);
+
+    textStyle(ITALIC);
+    textSize(32);
+    text("Loading word and setting up gallows...", screenWidth/2, screenHeight/2);
+    textStyle(NORMAL);
+
+  } else if (player.userType == "chooser") {
+
+  }
+}
+
+
+// Keyboard Input ///////////////////////////////////////////////////////////////////
 
 
 function keyPressed() {
@@ -117,16 +163,38 @@ function textModify(text, maxStringLength) {
 }
 
 
+// Jquery Events ////////////////////////////////////////////////////////////////////
+
+
 $('#reset').click(function() {
   socket.emit('Reset');
+  player.resetPlayer();
+  $("#become-guesser").css("background-color", "transparent");
+  $("#become-chooser").css("background-color", "transparent");
+  $("#become-guesser").prop("disabled", false);
+  $("#become-chooser").prop("disabled", false);
 });
+
 
 $('#become-chooser').click(function() {
   socket.emit('Become Chooser');
+  if (player.playerName.trim().length > 0) {
+    player.becomeChooser();
+    $(this).css("background-color", "rgb(100,100,100)");
+    $(this).prop("disabled", true);
+    $("#become-guesser").prop("disabled", true);
+  }
 });
+
 
 $('#become-guesser').click(function() {
   socket.emit('Become Guesser');
+  if (player.playerName.trim().length > 0) {
+    player.becomeGuesser();
+    $(this).css("background-color", "rgb(100,100,100)");
+    $(this).prop("disabled", true);
+    $("#become-chooser").prop("disabled", true);
+  }
 });
 
 
