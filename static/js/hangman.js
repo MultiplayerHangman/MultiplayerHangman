5143becc798780b656da0661942df6dbbcfa41a8
MultiplayerHangman <ericliang@v1020-wn-109-237.campus-dynamic.uwaterloo.ca>
2018-03-04 17:34:02 -0500

    Merges

--- static/js/hangman.js
+++ static/js/hangman.js
@@ -432,7 +432,6 @@ $('#become-guesser').click(function() {
 
 
 $('#submit').click(function() {
-<<<<<<< HEAD
   if (titlescreen) {
     if (player.secretPhrase.length > 0) {
       socket.emit('secret_phrase_submit', {'secret': player.secretPhrase});
@@ -446,12 +445,11 @@ $('#submit').click(function() {
     } else {
       alert("Please enter a letter.");
     }
-=======
+  }
   if (player.secretPhrase.length > 0) {
     socket.emit('secret_phrase_submit', {'secret': player.secretPhrase});
   } else {
     alert("Please enter a word.");
->>>>>>> 32bbe130c3b90592cde09a2769141e0fb34276cc
   }
 });
 
